9424f46c040550930ced4e2c652fe0a04d96f610
Alexandru <daniliuc.alexandru.alexandru@gmail.com>
2023-05-07 14:38:28 +0300

    adapting web
    centered status;
    update counter
    update first popup

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const movements = document.querySelector('#movements');
 const status = document.querySelector('.status');
 const field = document.querySelector('.field');
 const ball = document.getElementById('ball');
+const maxDimension = Math.max(field.clientWidth, field.clientHeight);
 
 let counter = 0;
 let crosses = [];
@@ -16,7 +17,7 @@ const positionBall = (length, ballRadius) => Math.trunc((Math.random() * (+lengt
 
 const starting = function (e) {
     e.preventDefault();
-    const ballR = Math.ceil(window.innerWidth / 320);
+    const ballR = Math.ceil(maxDimension / 300);
     start.classList.toggle('hidden');
     ball.style.width = `${ballR}rem`;
     positionBallWidth = positionBall(field.clientWidth, ballR);
@@ -26,7 +27,7 @@ const starting = function (e) {
     ball.style.left = `${percentPositionBallWidth}%`;
     ball.style.top = `${percentPositionBallHeight}%`;
     counter = 0;
-    movements.innerHTML = counter;
+    movements.innerHTML = String(counter).padStart(2, '0');
     status.innerHTML = 'GO';
     ball.style.backgroundColor = 'inherit'
     ball.style.zIndex = '1';
@@ -37,7 +38,7 @@ const createCross = function (x, y) {
     const newCross = document.createElement('p')
     newCross.style.left = `${x}px`;
     newCross.style.top = `${y}px`;
-    newCross.style.height = `${parseInt(ball.style.width) / 3}rem`;
+    newCross.style.height = `${parseInt(ball.style.width) / 2}rem`;
     field.prepend(newCross);
     return newCross
 };
@@ -48,7 +49,6 @@ field.addEventListener('click', function (e) {
     let positionCursorHeight = +e.offsetY;
 
     const positonDevHelper = (dev) => {
-        const maxDimension = Math.max(field.clientWidth, field.clientHeight);
         return positionCursorWidth <= (positionBallWidth + (maxDimension / dev)) &&
             positionCursorWidth >= (positionBallWidth - (maxDimension / dev)) &&
             positionCursorHeight <= (positionBallHeight + (maxDimension / dev)) &&
@@ -56,7 +56,7 @@ field.addEventListener('click', function (e) {
     };
     if (e.target.id === '') {
         counter++;
-        movements.innerHTML = counter;
+        movements.innerHTML = String(counter).padStart(2, '0');
 
         if (positonDevHelper(10)) {
             status.innerHTML = `HOT 🥵`;
@@ -75,7 +75,7 @@ field.addEventListener('click', function (e) {
 
     if (e.target.id === 'ball') {
         counter++;
-        movements.innerHTML = counter;
+        movements.innerHTML = String(counter).padStart(2, '0');
         start.classList.toggle('hidden');
         start.firstElementChild.innerHTML = `Congratulation ${counter} move${counter === 1 ? '' : 's'}`;
         status.innerHTML = `🥳`;
